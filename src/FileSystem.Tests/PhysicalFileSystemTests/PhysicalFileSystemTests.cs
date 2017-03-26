0104401560483cd7db01dae254e7a57a5ee68463
andy <andy@andydote.co.uk>
2017-03-26 16:32:12 +0300

    projects: add test project

---
+++ src/FileSystem.Tests/PhysicalFileSystemTests/PhysicalFileSystemTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystem.Tests.PhysicalFileSystemTests
+{
+	public abstract class PhysicalFileSystemTests : IDisposable
+	{
+		protected string Root { get; }
+		protected PhysicalFileSystem Fs { get; }
+
+		public PhysicalFileSystemTests()
+		{
+			Root = Guid.NewGuid().ToString();
+
+			Fs = new PhysicalFileSystem();
+		}
+
+		public virtual void Dispose()
+		{
+			try
+			{
+				Directory.Delete(Root, true);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Unable to Delete test directory");
+			}
+		}
+	}
+}
---
+++ src/FileSystem.Tests/Scratchpad.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace FileSystem.Tests
+{
+	public class Scratchpad
+	{
+		[Fact]
+		public void When_testing_something()
+		{
+
+		}
+	}
+}
