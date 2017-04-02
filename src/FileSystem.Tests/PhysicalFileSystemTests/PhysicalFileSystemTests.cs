334c7dbfe776a53c419c4a448d00baa87a9a07d5
andy <andy@andydote.co.uk>
2017-04-02 21:06:13 +0300

    physicalFileSystemTests: implement from fileSystemTests

---
+++ src/FileSystem.Tests/PhysicalFileSystemTests/PhysicalFileSystemTests.cs
@@ -0,0 +1,9 @@
+﻿namespace FileSystem.Tests.PhysicalFileSystemTests
+{
+	public class PhysicalFileSystemTests : FileSystemTests
+	{
+		public PhysicalFileSystemTests() : base(new PhysicalFileSystem())
+		{
+		}
+	}
+}
