99f7df0df4fb6a5885f5e1102e06ec75cfe2fc1f
andy <andy@andydote.co.uk>
2017-04-04 19:35:22 +0300

    extensions: add .ToSentence

---
+++ src/FileSystem.Tests/Internal/ExtensisonsTests.cs
@@ -0,0 +1,19 @@
+﻿using FileSystem.Internal;
+using Shouldly;
+using Xunit;
+
+namespace FileSystem.Tests.Internal
+{
+	public class ExtensisonsTests
+	{
+		[Theory]
+		[InlineData("FileCreated", "File Created")]
+		[InlineData("ThisHasASingleLetter", "This Has A Single Letter")]
+		[InlineData("ThisIDProperty", "This ID Property")]
+		[InlineData("ThisTLAToo", "This TLA Too")]
+		public void When_sentencing_a_string(string input, string expected)
+		{
+			input.ToSentence().ShouldBe(expected);
+		}
+	}
+}
---
+++ src/FileSystem/Internal/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FileSystem.Internal
+{
+	public static class Extensions
+	{
+		private static readonly Regex Expression = new Regex(
+			@"(?<=[A-Z])(?=[A-Z][a-z]) | (?<=[^A-Z])(?=[A-Z]) | (?<=[A-Za-z])(?=[^A-Za-z])",
+			RegexOptions.IgnorePatternWhitespace);
+
+		public static string ToSentence(this string value)
+		{
+			return Expression.Replace(value, " ");
+		}
+	}
+}
