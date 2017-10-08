2e591a7d47a2db09d065d476d253d44057384362
andy <andy@andydote.co.uk>
2017-10-08 21:10:46 +0300

    build: improve build script

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 
 # First parameter is build mode, defaults to Debug
-
 MODE=${1:-Debug}
-NAME="FileSystem"
+
+# Find the solution file in the root take it's name
+NAME=$(basename $(ls *.sln | head -n 1) .sln)
 
 dotnet restore
 dotnet build --configuration $MODE
-(cd ./src/FileSystem.Tests/ && dotnet xunit)
+
+find ./src -iname "*.Tests.csproj" -type f -exec dotnet test "{}" --configuration $MODE \;
 
 dotnet pack ./src/$NAME --configuration $MODE --output ../../.build
